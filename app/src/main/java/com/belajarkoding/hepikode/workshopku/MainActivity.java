c01cdd0eb674ff5678f60e7a13dfa690503292c3
zuhaerini <andizuhaerinirr@gmail.com>
2017-01-13 17:32:51 +0800

    Perbaiki forceclose pake try catch

--- app/src/main/java/com/belajarkoding/hepikode/workshopku/MainActivity.java
+++ app/src/main/java/com/belajarkoding/hepikode/workshopku/MainActivity.java
@@ -2,9 +2,11 @@ package com.belajarkoding.hepikode.workshopku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText Panjang = (EditText) findViewById(R.id.edt_panjang);
-        EditText Lebar = (EditText) findViewById(R.id.edt_lebar);
+        final EditText Panjang = (EditText) findViewById(R.id.edt_panjang);
+        final EditText Lebar = (EditText) findViewById(R.id.edt_lebar);
         Button Hitung = (Button) findViewById(R.id.btn_hitung);
-        TextView Hasil = (TextView) findViewById(R.id.txt_hasil);
+        final TextView Hasil = (TextView) findViewById(R.id.txt_hasil);
+
+        Hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    int panjang = Integer.valueOf(Panjang.getText().toString());
+                    int lebar = Integer.valueOf(Lebar.getText().toString());
+
+                    int luas = panjang * lebar;
+
+                    Hasil.setText(String.valueOf(luas));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Input Panjang dan lebar", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
